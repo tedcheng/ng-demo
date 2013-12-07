74f396d21f0d8c72e1d8c5f1b82e15035982f207
Ted Cheng <yiqun.cheng@gmail.com>
2013-12-06 23:51:02 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NgDemo::Application.config.secret_token = '7260f7cfdbfbdaa005c9e49a5ca7b7a4f6433154d5d574e37f79b11041b77df509e4c1b8cda26d9e69b2cfd43e88945e7eae81b05d93fe498ca4cfe323a2b4ea'
